36a7b7d9888e14a822b2d9648bf0026c3f23a564
KRJaeh0 <145985933+JJH0204@users.noreply.github.com>
2026-01-21 22:25:03 +0900

    CleanUp Dir

--- Branch/Assets/_Project/01. Scripts/Player/Parameters/Damagable/IDamagable.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public interface IDamagable
-{
-    public void ApplyDamage(float inDamage, LayerMask targetMask = default, float unitOfTime = 1.0f, float defenceIgnoreRate = 0.0f);
-}
